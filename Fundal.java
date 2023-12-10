9c3466e16705bf674436f6ed9cf23b6d7d7f3225
HojdaAdelin <adelinionut297@gmail.com>
2023-12-10 10:39:26 +0200

    New galaxy

--- Fundal.java
+++ Fundal.java
@@ -5,7 +5,7 @@ public class Fundal extends MyWorld
     public Button button;
     public Fundal()
     {    
-        GreenfootImage backgroundImage = new GreenfootImage("background.jpg");
+        GreenfootImage backgroundImage = new GreenfootImage("main-menu.jpg");
         backgroundImage.scale(getWidth(), getHeight());
         setBackground(backgroundImage);
         
